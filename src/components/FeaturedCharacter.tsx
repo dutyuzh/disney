88a3eab549647d9e0d252323ea9cc037fcb026b8
Dasha Utyuzh <dutyuzh@gmail.com>
2024-11-19 19:48:21 -0500

    update featured component mobile view

--- src/components/FeaturedCharacter.tsx
+++ src/components/FeaturedCharacter.tsx
@@ -12,7 +12,7 @@ const FeaturedCharacter: React.FC<FeaturedCharacterProps> = ({ characters }) => 
         <h2>Featured Characters!</h2>
       </div>
 
-      <div className="grid grid-cols-4 sm:grid-cols-2 lg:grid-cols-3 gap-8" >
+      <div className="grid grid-cols-4 sm:grid-cols-1 lg:grid-cols-2 gap-8" >
         {characters.map((character) => (
           <CharacterCard
             key={character._id}
