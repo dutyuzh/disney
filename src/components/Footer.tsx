fed0634e78bbe5cdf097c6e71e14d11a44ee01aa
Dasha Utyuzh <dutyuzh@gmail.com>
2024-11-19 19:47:29 -0500

    update the font size in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer: React.FC = () => {
           <Logo />
         </Link>
 
-        <div className="flex items-center py-2 mx-2 font-normal text-xs">
+        <div className="flex items-center py-4 mx-2 font-normal text-xs">
           For educational use only. All characters and content are the property of Disney. This test is for private use and development testing only and should not be distributed for public consumption
         </div>
       </Layout>
